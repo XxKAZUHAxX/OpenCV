8e8681967fc6dbd3f0cceed296f729275aabb5fa
XxKAZUHAxX <jesryljade18@gmail.com>
2023-03-08 18:46:57 +0800

    modified:   src/HandTracking/__pycache__/hand_tracking_module.cpython-37.pyc 	new file:   src/HandTracking/volume_hand_control.py

---
+++ src/HandTracking/volume_hand_control.py
@@ -0,0 +1,27 @@
+import cv2
+import mediapipe as mp
+import numpy as np
+import time
+import hand_tracking_module as htm
+
+cap = cv2.VideoCapture(0)
+cap.set(3, 1980)
+cap.set(4, 1080)
+pTime = 0
+
+detector = htm.handDetector()
+
+while True:
+    success, img = cap.read()
+
+    img = detector.findHands(img)
+    
+    cTime = time.time()
+    fps = 1/(cTime - pTime)
+    pTime = cTime
+    cv2.putText(img, f'FPS: {int(fps)}', (40,50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,255,0), 3)
+    cv2.imshow('Image', img)
+    if cv2.waitKey(1) & 0xFF == 27:
+        break
+
+cv2.destroyAllWindows()
